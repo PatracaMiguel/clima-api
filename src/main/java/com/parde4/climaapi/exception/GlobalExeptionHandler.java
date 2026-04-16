6b68b7a6df73b098d1063858650654e226ac4f5e
Nicole-Paris <zs23017371@estudiantes.uv.mx>
2026-04-15 21:09:16 -0600

    Usuarios listos

--- src/main/java/com/parde4/climaapi/exception/GlobalExeptionHandler.java
+++ src/main/java/com/parde4/climaapi/exception/GlobalExeptionHandler.java
@@ -1,5 +1,42 @@
 package com.parde4.climaapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExeptionHandler {
-    
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> manejarRuntime(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Error de negocio");
+        error.put("mensaje", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errores.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> manejarGeneral(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Error interno");
+        error.put("mensaje", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
--- src/main/java/com/parde4/climaapi/model/Usuario.java
+++ src/main/java/com/parde4/climaapi/model/Usuario.java
@@ -1,5 +1,33 @@
 package com.parde4.climaapi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idusuario", nullable = false)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 45)
+    private String nombre;
+
+    @Column(name = "correo", nullable = false, length = 45)
+    private String correo;
+
+    @Column(name = "contrasena", nullable = false, length = 60)
+    private String contrasena;
+
+    @Column(name = "fecha_creado")
+    private LocalDateTime fechaCreado;
 }
