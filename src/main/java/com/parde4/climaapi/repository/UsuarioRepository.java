61152a1491873275e3986e54a95b3a0ef7ecde6e
Miguel <miguelangelpatracalagunes@gmail.com>
2026-05-05 21:29:27 -0600

    Creación del endpoint Autenticación

--- src/main/java/com/parde4/climaapi/repository/UsuarioRepository.java
+++ src/main/java/com/parde4/climaapi/repository/UsuarioRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Usuario findByCorreo(String correo);
+    
 } 
